3fa53f3677fd6862c66c703eeeb55cd47eb62dae
Dusia Afanasyeva <dusiafa@MacBook-Pro-Dusia.local>
2024-04-18 13:53:25 +0300

    added changes to App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -5,14 +5,15 @@ import io.javalin.Javalin;
 public class App {
 
     public static Javalin getApp() {
+        int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "7070"));
+
         Javalin app = Javalin.create(/*config*/)
                 .get("/", ctx -> ctx.result("Hello World"))
-                .start(7070);
+                .start(port); // Используем значение порта
         return app;
     }
 
     public static void main(String[] args) {
         getApp();
     }
-
 }
