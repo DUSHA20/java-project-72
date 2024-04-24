1383869addf395d2a55f02ff41e465c25f47174f
Dusia Afanasyeva <dusiafa@MacBook-Pro-Dusia.local>
2024-04-24 12:01:25 +0300

    something added on 3 step

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -23,17 +23,25 @@ public class App {
     }
 
     public static Javalin getApp() {
+
         int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "7070"));
 
-        // Получаем URL базы данных из переменной окружения
-        String jdbcUrl = System.getenv("JDBC_DATABASE_URL");
+        // Получаем значения переменных окружения
+        String hostname = System.getenv("HOST");
+        String dbPort = "5432"; // Жестко закодированный порт PostgreSQL
+        String database = System.getenv("DATABASE");
+        String username = System.getenv("USERNAME");
+        String password = System.getenv("PASSWORD");
 
-        // Если переменная окружения не установлена, используем URL для базы данных H2 в памяти
-        if (jdbcUrl == null || jdbcUrl.isEmpty()) {
-            jdbcUrl = "jdbc:h2:mem:project";
-        }
+        // Формируем строку подключения
+        String jdbcUrl = String.format("jdbc:postgresql://%s:%s/%s?password=%s&user=%s",
+                hostname,
+                dbPort,
+                database,
+                password,
+                username);
 
-        // Конфигурация HikariCP для использования базы данных
+        // Конфигурируем HikariCP для использования базы данных
         HikariConfig config = new HikariConfig();
         config.setJdbcUrl(jdbcUrl);
         DataSource dataSource = new HikariDataSource(config);
